3eb5e0eb8f96bee753d1a77ff0182750d50e688c
Phanh <ho.x.hung.16@gmail.com>
2021-11-04 16:55:20 +0700

    Updated

--- javascript.js
+++ javascript.js
@@ -1,28 +1,13 @@
-// var modal=document.getElementById("modal-shopping");
-// var btnshopping=document.getElementById("btn-shopping");
-// var btntimes=document.getElementById("close");
-// var search=document.getElementById("search");
-// var btnclosesearch=document.getElementById("close-search");
-// var searchnavbar=document.getElementById("search-icon-nav");
-// var btnbars=document.getElementById("btn-bars");
-// var menunavbar=document.getElementById("menu-nav");
-// var btnclosemenu=document.getElementById("btn-close-menu");
-// btnshopping.onclick = function(){
-//     modal.style.display = "block";
-// }
-// btntimes.onclick=function(){
-//     modal.style.display="none";
-// }
-// btnclosesearch.onclick = function(){
-//     search.style.display="none";
-// }
-// searchnavbar.onclick = function(){
-//     search.style.display="block";
-//     search.style.display="flex";
-// }
-// btnbars.onclick=function(){
-//     menunavbar.style.display="block";
-// }
-// btnclosemenu.onclick=function(){
-//     menunavbar.style.display="none";
-// }
+window.onscroll= function() {scrollFunction()};
+function scrollFunction(){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
+    {
+        document.getElementById("navbar").style.backgroundColor="black";
+        document.getElementById("navbar-mobile").style.backgroundColor="black";
+    }
+    else 
+    {
+        document.getElementById("navbar").style.background="none";
+        document.getElementById("navbar-mobile").style.background="none";
+    }
+}
