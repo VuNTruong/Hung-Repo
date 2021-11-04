0da22822d832c94710a8659f94c7f84de667f715
Phanh <ho.x.hung.16@gmail.com>
2021-11-04 16:00:02 +0700

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,28 @@
+// var modal=document.getElementById("modal-shopping");
+// var btnshopping=document.getElementById("btn-shopping");
+// var btntimes=document.getElementById("close");
+// var search=document.getElementById("search");
+// var btnclosesearch=document.getElementById("close-search");
+// var searchnavbar=document.getElementById("search-icon-nav");
+// var btnbars=document.getElementById("btn-bars");
+// var menunavbar=document.getElementById("menu-nav");
+// var btnclosemenu=document.getElementById("btn-close-menu");
+// btnshopping.onclick = function(){
+//     modal.style.display = "block";
+// }
+// btntimes.onclick=function(){
+//     modal.style.display="none";
+// }
+// btnclosesearch.onclick = function(){
+//     search.style.display="none";
+// }
+// searchnavbar.onclick = function(){
+//     search.style.display="block";
+//     search.style.display="flex";
+// }
+// btnbars.onclick=function(){
+//     menunavbar.style.display="block";
+// }
+// btnclosemenu.onclick=function(){
+//     menunavbar.style.display="none";
+// }
